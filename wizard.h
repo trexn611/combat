efc2c74f35c35b4e8b789be2b8576fddeb76fb40
trexn611 <137101717+trexn611@users.noreply.github.com>
2025-01-12 21:15:19 -0800

    Update wizard.h

--- wizard.h
+++ wizard.h
@@ -1,1 +1,18 @@
+#include <string>
+#include "Character.h"
 
+using namespace std;
+
+#ifndef __WIZARD_H__
+#define __WIZARD_H__
+
+class Wizard : public Character{
+ private:
+	int rank; 
+
+ public:
+ 	Wizard(const string &name, double health, double attackStrength, int rank); 
+ 	void attack(Character& opponent) override; //override attack
+ };
+
+#endif
